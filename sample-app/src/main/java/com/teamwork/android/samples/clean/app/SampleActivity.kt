eb4efe253270024507d172b1b61425285300cc5f
Marco Salis <fast3r@gmail.com>
2018-04-24 14:09:08 +0100

    Modules: added sample-app-core and sample-app-feature1

--- sample-app/src/main/java/com/teamwork/android/samples/clean/app/SampleActivity.kt
+++ sample-app/src/main/java/com/teamwork/android/samples/clean/app/SampleActivity.kt
@@ -1,10 +1,11 @@
-package com.teamwork.android.samples.clean
+package com.teamwork.android.samples.clean.app
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.teamwork.android.samples.clean.R
 
 import kotlinx.android.synthetic.main.activity_sample.*
 
